0e28e57b704d6270d453a795539a9b8cd092b57f
Alexandra Corremans <alexandra.corremans@gmail.com>
2018-10-11 10:59:00 +1100

    build_tree

--- bst.rb
+++ bst.rb
@@ -13,45 +13,50 @@ end
 
 class BinarySearchTree
 
-    attr_reader :size
-
     def initialize(array)
         @tree = []
         @size = 0
-        create_tree(array)
+        build_tree(array)
     end
 
-    def create_tree(data)
+    def build_tree(data)
         data.each do |value|
             insert_node(value)
         end
     end
 
     def insert_node(value)
-        #if tree is empty: first node is root and gets a value
-        if (@tree.size == 0)
-            root = Node.new(value)
-            @tree.push(root)
-        
-        #one node already
-        else
+        if (@tree.size == 0) #empty tree: take first element as root
+            @root = Node.new(value)
+            @tree.push(@root)
+        else #compare new element with existing nodes
             new_node = Node.new(value)
-            @tree.map! do |node|
-                if value >= node.value
-                    node.right_child = new_node
-                    new_node.parent = node
-                else
-                    node.left_child = new_node
-                    new_node.parent = node
-                end
-            end
+            compare_nodes(@root, new_node)
             @tree.push(new_node)
         end
 
     end
 
+    def compare_nodes(node, new_node)
+        if new_node.value >= node.value
+            if node.right_child.nil?
+                node.right_child = new_node
+                new_node.parent = node
+            else
+                compare_nodes(node.right_child, new_node)
+            end
+        else
+            if node.left_child.nil?
+                node.left_child = new_node
+                new_node.parent = node
+            else
+                compare_nodes(node.left_child, new_node)
+            end
+        end
+    end
+
     def display
-        puts "Nodes"
+        puts "List of nodes in the tree"
         @tree.each do |node|
             puts "Value: #{node.value}"
             puts "Parent node value: #{node.parent.value}" unless node.parent.nil?
@@ -62,6 +67,6 @@ class BinarySearchTree
     end
 end
 
-array =  [1,7,4,23,8,9,4,3,5,7,9,67,6345,324]
-bst = BinarySearchTree.new([1,7])
+array =  [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+bst = BinarySearchTree.new(array)
 bst.display
