492c25a5d90aca9c6c40a80e2775ad56591c6f41
Alexandra Corremans <alexandra.corremans@gmail.com>
2018-10-14 13:22:51 +1100

    breadth first search

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-This is a Ruby implementation of a binary search tree using a `Node` and a `BinarySearchTree` class. The tree itself is an array of Node objects, each node linking to its parent and child nodes. 
+This is a Ruby implementation of a binary search tree using a `Node` and a `BinarySearchTree` class. The tree itself is an array of `Node` objects, each node linking to its parent and child nodes. 
 
-It builds a tree based on an unsorted array. I've assumed the array can't get sorted before the tree is built, meaning the tree can be unbalanced as per the assignment. 
+It builds a tree based on an unsorted array. As per the assignment, I've assumed the array can't get sorted before the tree is built, meaning the tree can be unbalanced. 
+
+To help check if the program works correctly, the `Node` class has a `#display` method which prints the node value and its parent and child nodes values (if any). Similarly the `#display` method for the `BinarySearchTree` class loops through the tree and displays each node in turn. 
+
+The `BinarySearchTree` class has the following search methods:
+
+  * `#breadth_first_search` takes a target value and returns the node at which it is located using the breadth first search technique
+
+If the target node value is not located, each of these methods returns `nil`. 
 
 From The Odin Project's [curriculum](https://www.theodinproject.com/courses/ruby-programming/lessons/data-structures-and-algorithms)
--- bst.rb
+++ bst.rb
@@ -9,6 +9,29 @@ class Node
         @right_child = nil
     end
 
+    def has_children?
+        !(@left_child.nil? && @right_child.nil?) ? true : false
+    end
+
+    def get_child_nodes
+        child_nodes = []
+        if @left_child.nil?
+            child_nodes.push(@right_child)
+        elsif @right_child.nil?
+            child_nodes.push(@left_child)
+        else
+            child_nodes.push(@left_child,@right_child)            
+        end
+        return child_nodes
+    end
+
+    def display
+        puts "Value: #{@value}"
+        puts "Parent node value: #{@parent.value}" unless @parent.nil?
+        puts "Left child node value: #{@left_child.value}" unless @left_child.nil?
+        puts "Right child node value: #{@right_child.value}" unless @right_child.nil?
+    end
+
 end
 
 class BinarySearchTree
@@ -58,20 +81,40 @@ class BinarySearchTree
     def display
         puts "List of nodes in the tree"
         @tree.each do |node|
-            puts "Value: #{node.value}"
-            puts "Parent node value: #{node.parent.value}" unless node.parent.nil?
-            puts "Left child node value: #{node.left_child.value}" unless node.left_child.nil?
-            puts "Right child node value: #{node.right_child.value}" unless node.right_child.nil?
-            puts "***********************"
+            node.display
+            puts "*******************"
         end
     end
 
-    def breadth_first_search
-        
+    def breadth_first_search(value)
+        return nil if @root.nil?
+        queue = []
+        current_node = @root
+        queue.push(current_node)
+        while not queue.empty?
+            if current_node.value == value
+                return current_node
+            else
+                queue = queue + current_node.get_child_nodes if current_node.has_children?
+                queue.shift
+                current_node = queue[0] unless queue.empty?
+            end
+        end
+        return nil
     end
 
 end
 
 array =  [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+puts "Creating tree..."
 bst = BinarySearchTree.new(array)
 bst.display
+
+puts "Breadth first search"
+found = bst.breadth_first_search(8)
+if found == nil
+    puts "Node not found"
+else
+    puts "Node found!"
+    found.display
+end
